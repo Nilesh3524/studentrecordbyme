5ff4691ed7a2f19e00cc628bbbe00c1ec1ea9854
Nilesh3524 <nileshnarule24@gmail.com>
2024-10-10 23:40:21 +0530

    colors done

--- src/main/java/com/record/student/controller/GlobalControllerAdvice.java
+++ src/main/java/com/record/student/controller/GlobalControllerAdvice.java
@@ -1,2 +1,15 @@
-package com.record.student.controller;public class GlobalControllerAdvice {
+package com.record.student.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.HandlerMapping;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("currentUrl")
+    public String addCurrentUrlToModel(HttpServletRequest request) {
+        return (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+    }
 }
